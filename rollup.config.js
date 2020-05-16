b7e5f94be2811c8d555d83069d853383ddedd825
Alex Lunyov <isuntc@gmail.com>
2020-05-16 19:29:04 +0800

    New build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@ import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'public/bundle.js',
+    file: 'public/assets/index.js',
     name: 'gradi',
     format: 'iife'
   },
@@ -16,9 +17,10 @@ export default {
     resolve(),
     commonjs(),
     isProduction && terser(),
+    !isProduction && serve('public'),
     svelte({
       css: function (css) {
-        css.write('public/main.css');
+        css.write('public/assets/index.css');
       },
 
       onwarn: (warning, handler) => {
